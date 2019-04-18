67778ec2411d2d2aa1b869e77068cb9b3edb2403
sunchula <sunchulapraveen@gmail.com>
2019-04-18 14:59:41 +0530

    Tested @RequestParm, @PathVariable

--- onlineshopping/src/main/java/net/praveen/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/net/praveen/onlineshopping/controller/PageController.java
@@ -1,7 +1,9 @@
 package net.praveen.onlineshopping.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -10,7 +12,29 @@ public class PageController {
 	@RequestMapping(value = {"/","/home","/index"})
 	public ModelAndView index() {
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting","weelcome to spring Web MVC");
+		mv.addObject("greeting","welcome to spring Web MVC");
+		return mv;
+	}
+	
+	@RequestMapping(value = "/test")
+	public ModelAndView test(@RequestParam(value= "greeting", required=false)String greeting) {
+		if(greeting==null) {
+			greeting="Hello this is Null Value";
+		}
+		
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting",greeting);
+		return mv;
+	}
+	
+	@RequestMapping(value = "/test/{greeting}")
+	public ModelAndView test1(@PathVariable("greeting")String greeting) {
+		if(greeting==null) {
+			greeting="Hello this is Null Value";
+		}
+		
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting",greeting);
 		return mv;
 	}
 }
